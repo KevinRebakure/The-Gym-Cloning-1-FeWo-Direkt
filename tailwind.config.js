60306f83ea74169612ee82e3a9bb174f16c81170
ElvisJackson1 <140909747+ElvisJackson1@users.noreply.github.com>
2024-04-04 17:02:27 +0200

    Working on the Hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  // content: ["./src/**/*.{html,js}"],
+  // content: ["'./**/*.html'"],
+  purge: {
+    content: ["./index.html"], // Add the path to your index.html file here
+  },
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
